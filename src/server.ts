8e3a552010b5307e77ae1c5b955039794c826950
Maxwell <Maxwellbarboza@hotmail.com>
2023-04-13 00:46:56 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import * as dotenv from 'dotenv'; 
-import express from "express";
+import express, { response } from "express";
 
 dotenv.config()
 const app = express();
 app.use(express.json());
 
+app.get("/", async (Request, Response) => {
 
+    return response.json("Entrei aqui.");
+
+})
 
 app.listen(process.env.PORT || 3000, () => console.log("Servidor subiu na porta 3000."));
 
