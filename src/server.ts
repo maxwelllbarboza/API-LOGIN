9c90528ea56ef16c8b1457c66c408a13df5497d6
Maxwell <Maxwellbarboza@hotmail.com>
2023-04-13 01:41:50 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -8,32 +8,7 @@ dotenv.config()
 const app = express();
 app.use(express.json());
 
-app.post('/usuarios', async (request, response) => {   
-    const { email, username, name } = request.body
-  
-    const verifyIfExistsUser = await prismaClient.products.findFirst({
-      where: {
-        username,
-        OR: {
-          email,
-        },
-      },
-    })
-  
-    if (verifyIfExistsUser)
-      return response.status(400).json({ error: 'Usuário já existe.' })
-  
-    const userCreated = await prismaClient.products.create({
-      data: {
-        name,
-        email,
-        username,
-        ,
-      },
-    })
-  
-    return response.json(userCreated)
-  })
+
 
 app.listen(port, () => console.log("Servidor subiu na porta 3000."));
 
