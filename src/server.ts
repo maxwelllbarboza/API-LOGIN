7ba57e881c6943c236c07c467fc94644d03f6762
Maxwell <Maxwellbarboza@hotmail.com>
2023-04-13 01:20:16 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,38 @@
 import * as dotenv from 'dotenv'; 
-import express, { response } from "express";
+import express from "express";
+import { prismaClient } from './database/client';
+
+const port = process.env.PORT || 3000
 
 dotenv.config()
 const app = express();
 app.use(express.json());
 
-app.get("/", async (Request, Response) => {
+app.post('/usuarios', async (request, response) => {   
+    const { email, username, name } = request.body
+  
+    const verifyIfExistsUser = await prismaClient.user.findFirst({
+      where: {
+        username,
+        OR: {
+          email,
+        },
+      },
+    })
+  
+    if (verifyIfExistsUser)
+      return response.status(400).json({ error: 'Usuário já existe.' })
+  
+    const userCreated = await prismaClient.user.create({
+      data: {
+        email,
+        username,
+        name,
+      },
+    })
+  
+    return response.json(userCreated)
+  })
 
-    return response.json("Entrei aqui.");
-
-})
-
-app.listen(process.env.PORT || 3000, () => console.log("Servidor subiu na porta 3000."));
+app.listen(port, () => console.log("Servidor subiu na porta 3000."));
 
